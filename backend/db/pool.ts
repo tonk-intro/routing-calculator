2bbfecb7c8a90abddd982a9c318330b9854cfe78
tonk-intro <gammelgul@gammelgul.de>
2025-03-30 14:12:15 +0200

    Docker env variables

--- backend/db/pool.ts
+++ backend/db/pool.ts
@@ -3,7 +3,7 @@ import pg from "pg";
 export default new pg.Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  database: process.env.DB__NAME,
+  database: process.env.DB_NAME,
   password: process.env.DB_PW,
   port: process.env.DB_PORT ? +process.env.DB_PORT : 4043,
 });
